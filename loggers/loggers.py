938bd6bffdbcb53c57b43dd5ed30ecceca27c7c0
Kay Kieran <kayp.kieran@gmail.com>
2020-08-21 23:25:32 +0700

    Update loggers.py
    add loggers

--- loggers/loggers.py
+++ loggers/loggers.py
@@ -3,23 +3,23 @@ import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
 
-
-
-
-
-
 class Logger():
     def __init__(self, log_dir='loggers/runs'):
         self.log_dir = log_dir
         if not os.path.exists(self.log_dir):
             os.mkdir(self.log_dir)
         self.writer = SummaryWriter(log_dir=self.log_dir)
+        self.iters = {}
+    def write(self, tags, values):
+        if not isinstance(tags, list):
+            tags = list(tags)
+        if not isinstance(values, list):
+            values = list(values)
 
-    def log(self, text):
-        for n_iter in range(100):
-            self.writer.add_scalar('Loss/train', np.random.random(), n_iter)
-            self.writer.add_scalar('Loss/test', np.random.random(), n_iter)
-            self.writer.add_scalar('Accuracy/train', np.random.random(), n_iter)
-            self.writer.add_scalar('Accuracy/test', np.random.random(), n_iter)
-        #self.writer.close()
+        for i, (tag, value) in enumerate(zip(tags,values)):
+            if tag not in self.iters.keys():
+                self.iters[tag] = 0
+            self.writer.add_scalar(tag, value, self.iters[tag])
+            self.iters[tag] += 1
+
 
