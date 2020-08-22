affa588c1cd696de014c87c415e5c8bf36f2dbd2
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2020-08-22 22:03:11 +0700

    fix retinanet

--- models/retinanet/loss.py
+++ models/retinanet/loss.py
@@ -3,104 +3,80 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-from .utils import one_hot_embedding
 from torch.autograd import Variable
 
 class FocalLoss(nn.Module):
-    def __init__(self, num_classes=20, device = None):
+    def __init__(self, num_classes):
         super(FocalLoss, self).__init__()
         self.num_classes = num_classes
-        self.device = torch.device("cuda" if device is not None else "cpu")
+
+    def _one_hot_embeding(self, labels):
+        """Embeding labels to one-hot form.
+        Args:
+            labels(LongTensor): class labels
+            num_classes(int): number of classes
+        Returns:
+            encoded labels, sized[N, #classes]
+        """
+
+        y = torch.eye(self.num_classes+1)  # [D, D]
+        return y[labels]  # [N, D]
 
     def focal_loss(self, x, y):
-        '''Focal loss.
-
+        """Focal loss
         Args:
-          x: (tensor) sized [N,D].
-          y: (tensor) sized [N,].
+            x(tensor): size [N, D]
+            y(tensor): size [N, ]
+        Returns:
+            (tensor): focal loss
+        """
 
-        Return:
-          (tensor) focal loss.
-        '''
         alpha = 0.25
         gamma = 2
 
-        t = one_hot_embedding(y.data.cpu(), 1+self.num_classes)  # [N,21]
-        t = t[:,1:]  # exclude background
-        t = Variable(t).to(self.device)  # [N,20]
+        t = self._one_hot_embeding(y.data.cpu())  # [N,21]
+        t = t[:, 1:]  # exclude background
+        t = Variable(t).cuda()  # [N,20]
 
-        p = x.sigmoid()
-        pt = p*t + (1-p)*(1-t)         # pt = p if t > 0 else 1-p
-        w = alpha*t + (1-alpha)*(1-t)  # w = alpha if t > 0 else 1-alpha
-        w = w * (1-pt).pow(gamma)
-        return F.binary_cross_entropy_with_logits(x, t, w, size_average=False)
+        logit = F.softmax(x,dim = 1)
+        logit = logit.clamp(1e-7, 1.-1e-7)
+        conf_loss_tmp = -1 * t.float() * torch.log(logit)
+        conf_loss_tmp = alpha * conf_loss_tmp * (1-logit)**gamma
+        conf_loss = conf_loss_tmp.sum()
 
-    def focal_loss_alt(self, x, y):
-        '''Focal loss alternative.
+        return conf_loss
 
+    def forward(self, loc_preds, cls_preds, loc_targets, cls_targets):
+        """Compute loss between (loc_preds, loc_targets) and (cls_preds, cls_targets).
         Args:
-          x: (tensor) sized [N,D].
-          y: (tensor) sized [N,].
-
-        Return:
-          (tensor) focal loss.
-        '''
-        alpha = 0.25
-
-        t = one_hot_embedding(y.data.cpu(), 1+self.num_classes)
-        t = t[:,1:]
-        t = Variable(t).to(self.device)
-
-        xt = x*(2*t-1)  # xt = x if t > 0 else -x
-        pt = (2*xt+1).sigmoid()
-
-        w = alpha*t + (1-alpha)*(1-t)
-        loss = -w*pt.log() / 2
-        return loss.sum()
-
-    def forward(self, loc_preds, loc_targets, cls_preds, cls_targets):
-        '''Compute loss between (loc_preds, loc_targets) and (cls_preds, cls_targets).
-
-        Args:
-          loc_preds: (tensor) predicted locations, sized [batch_size, #anchors, 4].
-          loc_targets: (tensor) encoded target locations, sized [batch_size, #anchors, 4].
-          cls_preds: (tensor) predicted class confidences, sized [batch_size, #anchors, #classes].
-          cls_targets: (tensor) encoded target labels, sized [batch_size, #anchors].
-
-        loss:
+          loc_preds(tensor): predicted locations, sized [batch_size, #anchors, 4].
+          loc_targets(tensor): encoded target locations, sized [batch_size, #anchors, 4].
+          cls_preds(tensor): predicted class confidences, sized [batch_size, #anchors, #classes].
+          cls_targets(tensor): encoded target labels, sized [batch_size, #anchors].
+        Returns:
           (tensor) loss = SmoothL1Loss(loc_preds, loc_targets) + FocalLoss(cls_preds, cls_targets).
-        '''
+        """
 
-
-
-        batch_size, num_boxes = cls_targets.size()
         pos = cls_targets > 0  # [N,#anchors]
         num_pos = pos.data.long().sum()
 
-        ################################################################
         # loc_loss = SmoothL1Loss(pos_loc_preds, pos_loc_targets)
-        ################################################################
-        mask = pos.unsqueeze(2).expand_as(loc_preds)       # [N,#anchors,4]
-        masked_loc_preds = loc_preds[mask].view(-1,4)      # [#pos,4]
-        masked_loc_targets = loc_targets[mask].view(-1,4)  # [#pos,4]
-        loc_loss = F.smooth_l1_loss(masked_loc_preds, masked_loc_targets, reduction='sum')
+        mask = pos.unsqueeze(2).expand_as(loc_preds)  # [N,#anchors,4]
+        masked_loc_preds = loc_preds[mask].view(-1, 4)  # [#pos,4]
+        masked_loc_targets = loc_targets[mask].view(-1, 4)  # [#pos,4]
+        loc_loss = F.smooth_l1_loss(masked_loc_preds, masked_loc_targets, reduction = 'sum')
 
-        ################################################################
         # cls_loss = FocalLoss(loc_preds, loc_targets)
-        ################################################################
         pos_neg = cls_targets > -1  # exclude ignored anchors
+        # num_pos_neg = pos_neg.data.long().sum()
         mask = pos_neg.unsqueeze(2).expand_as(cls_preds)
-        masked_cls_preds = cls_preds[mask].view(-1,self.num_classes)
-        cls_loss = self.focal_loss_alt(masked_cls_preds, cls_targets[pos_neg])
+        masked_cls_preds = cls_preds[mask].view(-1, self.num_classes)
 
-        # Divide all losses by the number of positives.
-        #cls_loss = cls_loss / num_pos if num_pos > 0 else 0
-        #loc_loss = loc_loss / num_pos if num_pos > 0 else 0
+        cls_loss = self.focal_loss(masked_cls_preds, cls_targets[pos_neg])
+        num_pos = max(1.0, num_pos.item())
 
-        loss = (cls_loss + loc_loss) / num_pos
+        #print('loc_loss: %.3f | cls_loss: %.3f' % (loc_loss.item() / num_pos, cls_loss.item() / num_pos), end=' | ')
+
+        loss = loc_loss / num_pos + cls_loss / num_pos
+
         return loss
-
-        # Loss keys:
-        # - C: Class Confidence Loss
-        # - B: Box Localization Loss
