91821884cb35e7a26f7e7b4b75e2bdd6309aad8c
Kay Kieran <kayp.kieran@gmail.com>
2020-08-22 09:24:06 +0700

    Update loggers.py

--- loggers/loggers.py
+++ loggers/loggers.py
@@ -1,11 +1,13 @@
 import os
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
-
+from datetime import datetime
 
 class Logger():
-    def __init__(self, log_dir='loggers/runs'):
+    def __init__(self, log_dir=None):
         self.log_dir = log_dir
+        if self.log_dir is None:
+            self.log_dir = os.path.join('loggers/runs',datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
         if not os.path.exists(self.log_dir):
             os.mkdir(self.log_dir)
         self.writer = SummaryWriter(log_dir=self.log_dir)
