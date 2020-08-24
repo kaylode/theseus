b75c1d23459db60f34ab8b092b0dc30324a6ba12
Kaylode Kieran <56054379+kaylode@users.noreply.github.com>
2020-08-24 15:08:11 +0700

    fix retinanet

---
+++ models/retinanet/__init__.py
@@ -0,0 +1,6 @@
+from .model import *
+from .encoder import *
+from .fpn import *
+from .retina_collator import *
+from .utils import *
+from .detector import *
--- models/retinanet/fpn.py
+++ models/retinanet/fpn.py
@@ -1,28 +1,28 @@
-'''RetinaFPN in PyTorch.'''
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from torch.autograd import Variable
 
 
-class Bottleneck(nn.Module):
+class Bottkeneck(nn.Module):
     expansion = 4
 
     def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        super(Bottkeneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=in_planes, out_channels=planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(num_features=planes)
+        self.conv2 = nn.Conv2d(in_channels=planes, out_channels=planes, kernel_size=3, stride=stride, padding=1,
+                               bias=False)
+        self.bn2 = nn.BatchNorm2d(num_features=planes)
+        self.conv3 = nn.Conv2d(in_channels=planes, out_channels=planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(num_features=planes * self.expansion)
 
         self.downsample = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion*planes:
+        if stride != 1 or in_planes != self.expansion * planes:
             self.downsample = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+                nn.Conv2d(in_channels=in_planes, out_channels=self.expansion * planes, kernel_size=1, stride=stride,
+                          bias=False),
+                nn.BatchNorm2d(num_features=self.expansion * planes)
             )
 
     def forward(self, x):
@@ -39,28 +39,29 @@ class FPN(nn.Module):
         super(FPN, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, padding=3, stride=2, bias=False)
+        self.bn1 = nn.BatchNorm2d(num_features=64)
 
         # Bottom-up layers
-        self.layer1 = self._make_layer(block,  64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.conv6 = nn.Conv2d(2048, 256, kernel_size=3, stride=2, padding=1)
-        self.conv7 = nn.Conv2d( 256, 256, kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, planes=64, num_blocks=num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, planes=128, num_blocks=num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, planes=256, num_blocks=num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, planes=512, num_blocks=num_blocks[3], stride=2)
+        self.conv6 = nn.Conv2d(in_channels=2048, out_channels=256, kernel_size=3, stride=2, padding=1)
+        self.conv7 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, stride=2, padding=1)
 
-        # Lateral layers
-        self.latlayer1 = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=0)
-        self.latlayer2 = nn.Conv2d(1024, 256, kernel_size=1, stride=1, padding=0)
-        self.latlayer3 = nn.Conv2d( 512, 256, kernel_size=1, stride=1, padding=0)
+        # lateral layers
+        self.lateral_layer1 = nn.Conv2d(in_channels=2048, out_channels=256, kernel_size=1)
+        self.lateral_layer2 = nn.Conv2d(in_channels=1024, out_channels=256, kernel_size=1)
+        self.lateral_layer3 = nn.Conv2d(in_channels=512, out_channels=256, kernel_size=1)
 
-        # Top-down layers
-        self.toplayer1 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
-        self.toplayer2 = nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1)
+        # smooth layers
+        self.smooth1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.smooth2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.smooth3 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1)
 
     def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1]*(num_blocks-1)
+        strides = [stride] + [1] * (num_blocks - 1)
         layers = []
         for stride in strides:
             layers.append(block(self.in_planes, planes, stride))
@@ -68,29 +69,17 @@ class FPN(nn.Module):
         return nn.Sequential(*layers)
 
     def _upsample_add(self, x, y):
-        '''Upsample and add two feature maps.
+        """
+        Upsample and add two feature maps
+        :param x: top feature map to be upsampled
+        :param y: lateral feature map
+        :return: added feature map
+        Use the 'bilinear' mode to upsample
+        """
+        _, _, H, W = y.size()
+        return F.interpolate(x, size=(H, W), mode='bilinear', align_corners=False) + y
+        #return F.upsample(x, size=(H, W), mode='nearest') + y
 
-        Args:
-          x: (Variable) top feature map to be upsampled.
-          y: (Variable) lateral feature map.
-
-        Returns:
-          (Variable) added feature map.
-
-        Note in PyTorch, when input size is odd, the upsampled feature map
-        with `F.upsample(..., scale_factor=2, mode='nearest')`
-        maybe not equal to the lateral feature map size.
-
-        e.g.
-        original input size: [N,_,15,15] ->
-        conv2d feature map size: [N,_,8,8] ->
-        upsampled feature map size: [N,_,16,16]
-
-        So we choose bilinear upsample which supports arbitrary output sizes.
-        '''
-        _,_,H,W = y.size()
-        return F.interpolate(x, size=(H,W), mode='bilinear', align_corners=False) + y
-               
     def forward(self, x):
         # Bottom-up
         c1 = F.relu(self.bn1(self.conv1(x)))
@@ -102,25 +91,21 @@ class FPN(nn.Module):
         p6 = self.conv6(c5)
         p7 = self.conv7(F.relu(p6))
         # Top-down
-        p5 = self.latlayer1(c5)
-        p4 = self._upsample_add(p5, self.latlayer2(c4))
-        p4 = self.toplayer1(p4)
-        p3 = self._upsample_add(p4, self.latlayer3(c3))
-        p3 = self.toplayer2(p3)
+        p5 = self.lateral_layer1(c5)
+        p4 = self._upsample_add(p5, self.lateral_layer2(c4))
+        p3 = self._upsample_add(p4, self.lateral_layer3(c3))
+        # smooth layer
+        p5 = self.smooth1(p5)
+        p4 = self.smooth2(p4)
+        p3 = self.smooth3(p3)
         return p3, p4, p5, p6, p7
 
 
-def FPN50():
-    return FPN(Bottleneck, [3,4,6,3])
+def getFPN(name):
+    if name =="FPN50":
+        return FPN(Bottkeneck, [3, 4, 6, 3])
+    elif name == 'FPN101':
+        return FPN(Bottkeneck, [2, 4, 23, 3])
+    else:
+        raise name +" is not implemented. Try using 'FPN50' or 'FPN101'" 
 
-def FPN101():
-    return FPN(Bottleneck, [2,4,23,3])
-
-
-def test():
-    net = FPN50()
-    fms = net(Variable(torch.randn(1,3,600,300)))
-    for fm in fms:
-        print(fm.size())
-
-# test()
