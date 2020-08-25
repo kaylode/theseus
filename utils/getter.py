2ea67e4e945eb7a40454c759c0e29d324df55740
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2020-08-25 08:18:25 +0700

    change directories

--- utils/getter.py
+++ utils/getter.py
@@ -6,3 +6,12 @@ from models import *
 from trainer import *
 from augmentations import *
 from loggers import *
+
+
+
+import torch
+from tqdm import tqdm
+import torch.nn as nn
+import torch.utils.data as data
+import torchvision.models as models
+from torch.optim.lr_scheduler import StepLR
