50f2fc5a78320d1bd1bee000374d8208c2ad55eb
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2020-08-25 08:15:53 +0700

    Update loggers.py
    add comments

--- loggers/loggers.py
+++ loggers/loggers.py
@@ -4,6 +4,10 @@ from torch.utils.tensorboard import SummaryWriter
 from datetime import datetime
 
 class Logger():
+    """
+    Logger for Tensorboard visualization
+    :param log_dir: Path to save checkpoint
+    """
     def __init__(self, log_dir=None):
         self.log_dir = log_dir
         if self.log_dir is None:
@@ -12,7 +16,13 @@ class Logger():
             os.mkdir(self.log_dir)
         self.writer = SummaryWriter(log_dir=self.log_dir)
         self.iters = {}
+
     def write(self, tags, values):
+        """
+        Write a log to specified directory
+        :param tags: (str) tag for log
+        :param values: (number) value for corresponding tag
+        """
         if not isinstance(tags, list):
             tags = list(tags)
         if not isinstance(values, list):
