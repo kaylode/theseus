f8d56161a4c41c971fd26a93889c49b3136eaa42
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2021-02-07 14:25:37 +0700

    add augmentations

--- augmentations/transforms.py
+++ augmentations/transforms.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
@@ -22,28 +23,27 @@ class Denormalize(object):
         img_show = np.clip(img_show,0,1)
         return img_show
 
-def get_resize_augmentation(image_size, keep_ratio=False):
+def get_resize_augmentation(image_size, keep_ratio=False, box_transforms = False):
+
+    bbox_params = A.BboxParams(
+                format='pascal_voc', 
+                min_area=0, 
+                min_visibility=0,
+                label_fields=['class_labels']) if box_transforms else None
+
     if not keep_ratio:
         return  A.Compose([
             A.Resize(
                 height = image_size[1],
                 width = image_size[0]
             )], 
-            bbox_params=A.BboxParams(
-                format='pascal_voc', 
-                min_area=0, 
-                min_visibility=0,
-                label_fields=['class_labels']))
+            bbox_params= bbox_params) 
     else:
         return A.Compose([
             A.LongestMaxSize(max_size=max(image_size)), 
-            A.PadIfNeeded(min_height=image_size[1], min_width=config.image_size[0], p=1.0, border_mode=cv2.BORDER_CONSTANT),
+            A.PadIfNeeded(min_height=image_size[1], min_width=image_size[0], p=1.0, border_mode=cv2.BORDER_CONSTANT),
             ], 
-            bbox_params=A.BboxParams(
-                format='pascal_voc', 
-                min_area=0, 
-                min_visibility=0,
-                label_fields=['class_labels']))
+            bbox_params=bbox_params)
         
 
 def get_augmentation(config, _type='train'):
