b2e11c76948c9dc1e6e8cf65c08b4461a92e099e
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2021-03-10 21:37:50 +0700

    add more configs modification

--- configs/configs.py
+++ configs/configs.py
@@ -10,3 +10,17 @@ class Config():
             return self._attr[attr]
         except KeyError:
             return None
+
+    def __str__(self):
+        print("##########   CONFIGURATION INFO   ##########")
+        pretty(self._attr)
+        return '\n'
+        
+def pretty(d, indent=0):
+  for key, value in d.items():
+    print('    ' * indent + str(key) + ':', end='')
+    if isinstance(value, dict):
+      print()
+      pretty(value, indent+1)
+    else:
+      print('\t' * (indent+1) + str(value))
