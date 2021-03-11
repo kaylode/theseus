934a45f1ddd2ba3863a42ce10b6f258083beade0
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2021-03-11 22:25:45 +0700

    turn off clahe

--- augmentations/transforms.py
+++ augmentations/transforms.py
@@ -55,7 +55,7 @@ def get_augmentation(config, _type='train'):
             A.Blur(blur_limit=3, p=0.1),
         ], p=0.3),
         A.ShiftScaleRotate(shift_limit=0.0625, scale_limit=0.2, rotate_limit=20, p=0.3),
-        A.CLAHE(clip_limit=2.0, tile_grid_size=(8,8), p=0.5),
+        # A.CLAHE(clip_limit=2.0, tile_grid_size=(8,8), p=0.5),
         A.OneOf([
             A.HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit= 0.2, 
                                  val_shift_limit=0.2, p=0.9),
