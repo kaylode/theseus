1e0f46cf95561efc6778be239fb73fa2956db21f
lannguyen <18120051@student.hcmus.edu.vn>
2021-04-07 17:15:27 +0700

    fix Gradcam when using DataParallel

--- utils/gradcam.py
+++ utils/gradcam.py
@@ -19,22 +19,23 @@ _MEAN = (0.485, 0.456, 0.406)
 _STD = [0.229, 0.224, 0.225]
 
 configs = {
-  "nfnet": {
-    'feature_module': {
-        'block_name': 'stages',
-        'block_index': 3
+    "nfnet": {
+        'feature_module': {
+            'block_name': 'stages',
+            'block_index': 3
+        },
+        'target_layer_names': "5"
     },
-    'target_layer_names': "5"
-  },
 
-  "efficientnet": {
-    'feature_module': {
-        'block_name': 'blocks',
-        'block_index': 6
-    },
-    'target_layer_names': "1"
-  }
+    "efficientnet": {
+        'feature_module': {
+            'block_name': 'blocks',
+            'block_index': 6
+        },
+        'target_layer_names': "1"
+    }
 }
+
 
 def show_cam_on_image(img, mask, label):
     heatmap = cv2.applyColorMap(np.uint8(255 * mask), cv2.COLORMAP_JET)
@@ -42,14 +43,15 @@ def show_cam_on_image(img, mask, label):
     cam = heatmap + np.float32(img)
     cam = cam / np.max(cam)
 
-    cv2.putText(cam, str(label), 
-      (10,500), 
-      cv2.FONT_HERSHEY_SIMPLEX, 
-      1,
-      (255,255,255),
-      2)
+    cv2.putText(cam, str(label),
+                (10, 500),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 255, 255),
+                2)
 
     return np.uint8(255 * cam)
+
 
 class FeatureExtractor():
     """ Class for extracting activations and
@@ -73,6 +75,7 @@ class FeatureExtractor():
                 outputs += [x]
         return outputs, x
 
+
 class ModelOutputs():
     """ Class for making a forward pass, and getting:
     1. The network output.
@@ -83,14 +86,23 @@ class ModelOutputs():
         self.model = model
         self.feature_module = feature_module
         self.feature_module_name = feature_module_name
-        self.feature_extractor = FeatureExtractor(self.feature_module, target_layers)
+        self.feature_extractor = FeatureExtractor(
+            self.feature_module, target_layers)
 
     def get_gradients(self):
         return self.feature_extractor.gradients
 
     def __call__(self, x):
         target_activations = []
-        for name, module in self.model._modules.items():
+
+        # Handle KeyError when using DataParallel
+        try:
+            self.model._modules[self.feature_module_name]
+            self.model_modules = self.model._modules
+        except KeyError:
+            self.model_modules = self.model._modules['module']._modules
+
+        for name, module in self.model_modules.items():
             if name == self.feature_module_name:
                 for name2, module2 in module._modules.items():
                     if module2 == self.feature_module:
@@ -102,6 +114,7 @@ class ModelOutputs():
 
         return target_activations, x
 
+
 class GradCam:
     def __init__(self, model, config_name):
         self.config_name = config_name
@@ -109,15 +122,21 @@ class GradCam:
         self.feature_module_config = configs[config_name]["feature_module"]
         self.feature_module_name = self.feature_module_config["block_name"]
 
-        self.feature_module = self.model._modules[self.feature_module_name]
+        # Handle KeyError when using DataParallel
+        try:
+            self.feature_module = self.model._modules[self.feature_module_name]
+        except KeyError:
+            self.feature_module = self.model._modules['module']._modules[self.feature_module_name]
+
         if "block_index" in self.feature_module_config.keys():
             block_index = int(self.feature_module_config["block_index"])
             self.feature_module = self.feature_module[block_index]
 
         self.target_layer_names = configs[config_name]["target_layer_names"]
         self.model.eval()
-        
-        self.extractor = ModelOutputs(self.model, self.feature_module, self.feature_module_name, self.target_layer_names)
+
+        self.extractor = ModelOutputs(
+            self.model, self.feature_module, self.feature_module_name, self.target_layer_names)
 
     def forward(self, input_img):
         return self.model(input_img)
@@ -127,13 +146,13 @@ class GradCam:
 
         if target_category == None:
             target_category = np.argmax(output.cpu().data.numpy())
-        
+
         one_hot = np.zeros((1, output.size()[-1]), dtype=np.float32)
         one_hot[0][target_category] = 1
         one_hot = torch.from_numpy(one_hot).requires_grad_(True)
-      
+
         one_hot = one_hot.cuda()
-        
+
         one_hot = torch.sum(one_hot * output)
 
         self.feature_module.zero_grad()
@@ -157,8 +176,9 @@ class GradCam:
         cam = cam / np.max(cam)
         return cam, int(target_category)
 
+
 def main(args, config):
-    
+
     transforms = A.Compose([
         A.Resize(config.image_size[0], config.image_size[1]),
         A.Normalize(mean=_MEAN, std=_STD, max_pixel_value=255.0, p=1.0),
@@ -175,7 +195,7 @@ def main(args, config):
     img_show = denom(img_tensor)
 
     net = BaseTimmModel(
-        name=config.model_name, 
+        name=config.model_name,
         num_classes=len(config.obj_list))
 
     if args.weight is not None:
@@ -187,14 +207,14 @@ def main(args, config):
                 net.load_state_dict(state["model"])
             except:
                 print("Cannot load weight")
-    
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
+
     net = net.to(device)
     img_tensor = img_tensor.to(device)
 
     config_name = config.model_name.split('_')[0]
-    grad_cam = GradCam(model=net, config_name= config_name)
+    grad_cam = GradCam(model=net, config_name=config_name)
 
     target_category = None
     grayscale_cam, label = grad_cam(img_tensor, target_category)
@@ -202,15 +222,18 @@ def main(args, config):
     label = config.obj_list[label]
     img_cam = show_cam_on_image(img_show, grayscale_cam, label)
     cv2.imwrite(args.image_out, img_cam)
-        
 
-if __name__=='__main__':
-    parser = argparse.ArgumentParser('Vizualize Gradient Class Activation Mapping')
-    parser.add_argument('config' , default='config', type=str, help='project file that contains parameters')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        'Vizualize Gradient Class Activation Mapping')
+    parser.add_argument('config', default='config', type=str,
+                        help='project file that contains parameters')
     parser.add_argument('--image', type=str, help='image to test Grad-CAM')
     parser.add_argument('--weight', type=str, help='weight to load to model')
-    parser.add_argument('--image_out', default='./cam.jpg' ,type=str, help='image to test Grad-CAM')
+    parser.add_argument('--image_out', default='./cam.jpg',
+                        type=str, help='image to test Grad-CAM')
     args = parser.parse_args()
-    config = Config(os.path.join('configs',args.config+'.yaml'))
+    config = Config(os.path.join('configs', args.config+'.yaml'))
 
     main(args, config)
