987fb25ab28b266628743a7bc62981e02b8c58f2
Kaylode Kieran <official_kaylode@users.noreply.github.com>
2021-04-11 14:46:41 +0700

    logging each run to each folder

--- loggers/loggers.py
+++ loggers/loggers.py
@@ -10,10 +10,9 @@ class Logger():
     """
     def __init__(self, log_dir=None):
         self.log_dir = log_dir
-        if self.log_dir is None:
-            self.log_dir = os.path.join('loggers/runs',datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+        self.log_dir = os.path.join(log_dir,datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
         if not os.path.exists(self.log_dir):
-            os.mkdir(self.log_dir)
+            os.makedirs(self.log_dir)
         self.writer = SummaryWriter(log_dir=self.log_dir)
         self.iters = {}
 
