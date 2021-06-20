24ab98043ae0ea8424c50e876c232666f12d12f8
kaylode <kaylode@github.com>
2021-06-20 23:59:26 +0700

    fix config

--- configs/config.py
+++ configs/config.py
@@ -3,28 +3,27 @@ import yaml
 class Config():
     def __init__(self, yaml_path):
         yaml_file = open(yaml_path)
-        self._attr = yaml.load(yaml_file, Loader=yaml.FullLoader)['settings']
+        _attr = yaml.load(yaml_file, Loader=yaml.FullLoader)['settings']
+        for key, value in _attr.items():
+            self.__dict__[key] = value
 
     def __setattr__(self, name, value):
         self.__dict__[name] = value
 
     def __getattr__(self, attr):
         try:
-            return self._attr[attr]
+            return self.__dict__[attr]
         except KeyError:
-            try:
-                return self.__dict__[attr]
-            except KeyError:
-                return None
+            return None
 
     def __str__(self):
         print("##########   CONFIGURATION INFO   ##########")
-        pretty(self._attr)
+        pretty(self.__dict__)
         return '\n'
     
     def to_dict(self):
         out_dict = {}
-        for k,v in self._attr.items():
+        for k,v in self.__dict__.items():
             if v is not None:
                 out_dict[k] = v
         return out_dict
@@ -33,7 +32,7 @@ class Config():
 def config_from_dict(_dict):
     config = Config('./configs/configs.yaml')
     for k,v in _dict.items():
-        config[k] = v
+        config.__setattr__(k,v)
     return config
         
 def pretty(d, indent=0):
