8f2e9fdc599ce28f78760d6e41a5265f3aa2eea8
kaylode <pmkhoi@selab.hcmus.edu.vn>
2021-07-08 01:27:51 +0700

    add resume logging

--- loggers/loggers.py
+++ loggers/loggers.py
@@ -1,14 +1,17 @@
 import os
-import numpy as np
-from torch.utils.tensorboard import SummaryWriter
+import glob
+import traceback
+import pandas as pd
 from datetime import datetime
+from torch.utils.tensorboard import SummaryWriter
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
 
 class Logger():
     """
     Logger for Tensorboard visualization
     :param log_dir: Path to save checkpoint
     """
-    def __init__(self, log_dir=None):
+    def __init__(self, log_dir=None, resume=None):
         self.log_dir = log_dir
         
         if self.log_dir is None:
@@ -17,6 +20,10 @@ class Logger():
         if not os.path.exists(self.log_dir):
             os.makedirs(self.log_dir)
         self.writer = SummaryWriter(log_dir=self.log_dir)
+
+        # Load old logging
+        if resume is not None:
+            self.load(resume)
 
     def write(self, tags, values, step):
         """
@@ -42,4 +49,58 @@ class Logger():
         """
 
         self.writer.add_figure(tag, image, global_step=step)
+
+    def load(self, old_log):
+        all_log = tflog2pandas(old_log)
+
+        for _, row in all_log.iterrows():
+            tag, value, step = row
+            self.writer.add_scalar(tag,value,step)
+
+
+def tflog2pandas(path: str) -> pd.DataFrame:
+    """convert single tensorflow log file to pandas DataFrame
+
+    Parameters
+    ----------
+    path : str
+        path to tensorflow log file
+
+    Returns
+    -------
+    pd.DataFrame
+        converted dataframe
+    """
+    DEFAULT_SIZE_GUIDANCE = {
+        "compressedHistograms": 1,
+        "images": 1,
+        "scalars": 0,  # 0 means load all
+        "histograms": 1,
+    }
+    runlog_data = pd.DataFrame({"metric": [], "value": [], "step": []})
+    try:
+        event_acc = EventAccumulator(path, DEFAULT_SIZE_GUIDANCE)
+        event_acc.Reload()
+        tags = event_acc.Tags()["scalars"]
+        # tags = event_acc.Tags()["images"]
+        for tag in tags:
+            event_list = event_acc.Scalars(tag)
+            values = list(map(lambda x: x.value, event_list))
+            step = list(map(lambda x: x.step, event_list))
+            r = {"metric": [tag] * len(step), "value": values, "step": step}
+            r = pd.DataFrame(r)
+            runlog_data = pd.concat([runlog_data, r])
+    # Dirty catch of DataLossError
+    except Exception:
+        print("Event file possibly corrupt: {}".format(path))
+        traceback.print_exc()
+    return runlog_data
+
+def find_old_log(weight_path):
+    pardir = os.path.dirname(weight_path)
+    event_paths = glob.glob(os.path.join(pardir, "event*"))
+    if len(event_paths) == 0:
+        return None
+    else:
+        return event_paths[0]
 
