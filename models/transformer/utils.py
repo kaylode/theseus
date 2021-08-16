0ba1bab2b70a7533bdf964cd931301b4da07bb54
Kaylode Kieran <pmkhoi@selab.hcmus.edu.vn>
2021-08-17 01:26:17 +0700

    fix return figs

--- models/transformer/utils.py
+++ models/transformer/utils.py
@@ -20,7 +20,7 @@ class PositionwiseFeedForward(nn.Module):
         """
         return self.w_2(self.dropout(F.relu(self.w_1(x))))
 
-def draw_attention_map(input, target, model, show_fig=True, save_fig=None):
+def draw_attention_map(input, target, model, show_fig=True, save_fig=None, return_figs=False):
     """
     Draw attention map from model, default: transformer
     :input:
@@ -39,15 +39,23 @@ def draw_attention_map(input, target, model, show_fig=True, save_fig=None):
         seaborn.heatmap(data, 
                         xticklabels=x, square=True, yticklabels=y, vmin=0.0, vmax=1.0, 
                         cbar=False, ax=ax)
-        
+    
+    if return_figs:
+        figs = []
+
     for layer in range(1, 6, 2):
-        print("Decoder Src Layer", layer+1)
+        # print("Decoder Src Layer", layer+1)
         fig, axs = plt.subplots(1,4, figsize=(20, 10))
         for h in range(4):
             draw(model.decoder.layers[layer].attn_2.attn[0, h].data.cpu()[:len(tgt_sent), :len(sent)], 
                 sent, tgt_sent if h ==0 else [], ax=axs[h])
         if show_fig:
             plt.show()
+        if return_figs:
+            figs.append([f"Layer {layer+1}", fig])
 
     if save_fig is not None:
         plt.savefig(save_fig)
+    
+    if return_figs:
+        return figs
