ca7f0276405976a40c2e6b3a34846043a0896793
lannguyen <18120051@student.hcmus.edu.vn>
2021-08-20 13:10:32 +0700

    fix cam in tensorboard

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import torch.nn as nn
+import matplotlib.pyplot as plt
 import torch
 from tqdm import tqdm
 from .checkpoint import Checkpoint
@@ -12,13 +13,14 @@ from torch.cuda import amp
 from utils.gradcam import GradCam, show_cam_on_image
 from augmentations import Denormalize
 
+
 class Trainer():
     def __init__(self,
-                config,
-                model, 
-                trainloader, 
-                valloader,
-                **kwargs):
+                 config,
+                 model,
+                 trainloader,
+                 valloader,
+                 **kwargs):
 
         self.cfg = config
         self.model = model
@@ -26,22 +28,20 @@ class Trainer():
         self.criterion = model.criterion
         self.trainloader = trainloader
         self.valloader = valloader
-        self.metrics = model.metrics #list of metrics
+        self.metrics = model.metrics  # list of metrics
         self.set_attribute(kwargs)
-        
-        
 
-    def fit(self, start_epoch = 0, start_iter = 0, num_epochs = 10 ,print_per_iter = None):
+    def fit(self, start_epoch=0, start_iter=0, num_epochs=10, print_per_iter=None):
         self.num_epochs = num_epochs
         self.num_iters = (num_epochs+1) * len(self.trainloader)
         if self.checkpoint is None:
-            self.checkpoint = Checkpoint(save_per_epoch = int(num_epochs/10)+1)
+            self.checkpoint = Checkpoint(save_per_epoch=int(num_epochs/10)+1)
 
         if print_per_iter is not None:
             self.print_per_iter = print_per_iter
         else:
             self.print_per_iter = int(len(self.trainloader)/10)
-        
+
         self.epoch = start_epoch
 
         # For one-cycle lr only
@@ -59,21 +59,20 @@ class Trainer():
                 if self.evaluate_per_epoch != 0:
                     if epoch % self.evaluate_per_epoch == 0 and epoch+1 >= self.evaluate_per_epoch:
                         self.evaluate_epoch()
-                        
+
                 if self.scheduler is not None and self.step_per_epoch:
                     self.scheduler.step()
                     lrl = [x['lr'] for x in self.optimizer.param_groups]
                     lr = sum(lrl) / len(lrl)
                     log_dict = {'Training/Learning rate': lr}
                     self.logging(log_dict, step=self.epoch)
-                
 
-            except KeyboardInterrupt:   
+            except KeyboardInterrupt:
                 self.checkpoint.save(
-                    self.model, 
-                    save_mode = 'last', 
-                    epoch = self.epoch, 
-                    iters = self.iters, 
+                    self.model,
+                    save_mode='last',
+                    epoch=self.epoch,
+                    iters=self.iters,
                     best_value=self.best_value,
                     class_names=self.trainloader.dataset.classes,
                     config=self.cfg)
@@ -90,68 +89,76 @@ class Trainer():
 
         self.optimizer.zero_grad()
         for i, batch in enumerate(self.trainloader):
-            
+
             start_time = time.time()
-        
+
             with amp.autocast(enabled=self.use_amp):
                 loss, loss_dict = self.model.training_step(batch)
                 if self.use_accumulate:
                     loss /= self.accumulate_steps
 
             self.model.scaler(loss, self.optimizer)
-            
+
             if self.use_accumulate:
                 if (i+1) % self.accumulate_steps == 0 or i == len(self.trainloader)-1:
-                    self.model.scaler.step(self.optimizer, clip_grad=self.clip_grad, parameters=self.model.parameters())
+                    self.model.scaler.step(
+                        self.optimizer, clip_grad=self.clip_grad, parameters=self.model.parameters())
                     self.optimizer.zero_grad()
 
                     if self.scheduler is not None and not self.step_per_epoch:
-                        self.scheduler.step((self.num_epochs + i) / len(self.trainloader))
+                        self.scheduler.step(
+                            (self.num_epochs + i) / len(self.trainloader))
                         lrl = [x['lr'] for x in self.optimizer.param_groups]
                         lr = sum(lrl) / len(lrl)
                         log_dict = {'Training/Learning rate': lr}
                         self.logging(log_dict, step=self.iters)
             else:
-                self.model.scaler.step(self.optimizer, clip_grad=self.clip_grad, parameters=self.model.parameters())
+                self.model.scaler.step(
+                    self.optimizer, clip_grad=self.clip_grad, parameters=self.model.parameters())
                 self.optimizer.zero_grad()
                 if self.scheduler is not None and not self.step_per_epoch:
                     # self.scheduler.step()
-                    self.scheduler.step((self.num_epochs + i) / len(self.trainloader))
+                    self.scheduler.step(
+                        (self.num_epochs + i) / len(self.trainloader))
                     lrl = [x['lr'] for x in self.optimizer.param_groups]
                     lr = sum(lrl) / len(lrl)
                     log_dict = {'Training/Learning rate': lr}
                     self.logging(log_dict, step=self.iters)
-                
+
             torch.cuda.synchronize()
 
             end_time = time.time()
 
-            for (key,value) in loss_dict.items():
+            for (key, value) in loss_dict.items():
                 if key in running_loss.keys():
                     running_loss[key] += value
                 else:
                     running_loss[key] = value
 
             running_time += end_time-start_time
-            self.iters = self.start_iter + len(self.trainloader)*self.epoch + i + 1
+            self.iters = self.start_iter + \
+                len(self.trainloader)*self.epoch + i + 1
             if self.iters % self.print_per_iter == 0:
-                
+
                 for key in running_loss.keys():
                     running_loss[key] /= self.print_per_iter
                     running_loss[key] = np.round(running_loss[key], 5)
-                loss_string = '{}'.format(running_loss)[1:-1].replace("'",'').replace(",",' ||')
-                print("[{}|{}] [{}|{}] || {} || Time: {:10.4f}s".format(self.epoch, self.num_epochs, self.iters, self.num_iters,loss_string, running_time))
-                self.logging({"Training/Batch Loss" : running_loss['T']/ self.print_per_iter,}, step=self.iters)
+                loss_string = '{}'.format(running_loss)[
+                    1:-1].replace("'", '').replace(",", ' ||')
+                print("[{}|{}] [{}|{}] || {} || Time: {:10.4f}s".format(
+                    self.epoch, self.num_epochs, self.iters, self.num_iters, loss_string, running_time))
+                self.logging(
+                    {"Training/Batch Loss": running_loss['T'] / self.print_per_iter, }, step=self.iters)
                 running_loss = {}
                 running_time = 0
 
             if (self.iters % self.checkpoint.save_per_iter == 0 or self.iters == self.num_iters - 1):
                 print(f'Save model at [{self.epoch}|{self.iters}] to last.pth')
                 self.checkpoint.save(
-                    self.model, 
-                    save_mode = 'last', 
-                    epoch = self.epoch, 
-                    iters = self.iters, 
+                    self.model,
+                    save_mode='last',
+                    epoch=self.epoch,
+                    iters=self.iters,
                     best_value=self.best_value,
                     class_names=self.trainloader.dataset.classes,
                     config=self.cfg)
@@ -166,8 +173,8 @@ class Trainer():
         with torch.no_grad():
             for batch in tqdm(self.valloader):
                 loss, loss_dict = self.model.evaluate_step(batch)
-                
-                for (key,value) in loss_dict.items():
+
+                for (key, value) in loss_dict.items():
                     if key in epoch_loss.keys():
                         epoch_loss[key] += value
                     else:
@@ -181,17 +188,21 @@ class Trainer():
         for key in epoch_loss.keys():
             epoch_loss[key] /= len(self.valloader)
             epoch_loss[key] = np.round(epoch_loss[key], 5)
-        loss_string = '{}'.format(epoch_loss)[1:-1].replace("'",'').replace(",",' ||')
+        loss_string = '{}'.format(epoch_loss)[
+            1:-1].replace("'", '').replace(",", ' ||')
         print()
-        print("[{}|{}] || {} || Time: {:10.4f} s".format(self.epoch, self.num_epochs, loss_string, running_time))
+        print("[{}|{}] || {} || Time: {:10.4f} s".format(
+            self.epoch, self.num_epochs, loss_string, running_time))
 
         for metric, score in metric_dict.items():
-            print(metric +': ' + str(score), end = ' | ')
+            print(metric + ': ' + str(score), end=' | ')
         print()
         print('==========================================================================')
 
-        log_dict = {"Validation/Epoch Loss" : epoch_loss['T'] / len(self.valloader),}
-        metric_log_dict = {f"Validation/{k}":v for k,v in metric_dict.items()}
+        log_dict = {
+            "Validation/Epoch Loss": epoch_loss['T'] / len(self.valloader), }
+        metric_log_dict = {f"Validation/{k}": v for k,
+                           v in metric_dict.items()}
         log_dict.update(metric_log_dict)
         self.logging(log_dict, step=self.epoch)
 
@@ -199,10 +210,10 @@ class Trainer():
         if metric_dict['acc'] > self.best_value:
             self.best_value = metric_dict['acc']
             self.checkpoint.save(
-                self.model, 
-                save_mode = 'best', 
-                epoch = self.epoch, 
-                iters = self.iters, 
+                self.model,
+                save_mode='best',
+                epoch=self.epoch,
+                iters=self.iters,
                 best_value=self.best_value,
                 class_names=self.trainloader.dataset.classes,
                 config=self.cfg)
@@ -222,10 +233,11 @@ class Trainer():
         self.model.eval()
 
         config_name = self.cfg.model_name.split('_')[0]
-        grad_cam = GradCam(model=self.model.model, config_name= config_name)
+        grad_cam = GradCam(model=self.model.model, config_name=config_name)
 
         for idx, inputs in enumerate(images):
-            image_outname = os.path.join('samples', f'{self.epoch}_{self.iters}_{idx}.jpg')
+            image_outname = os.path.join(
+                'samples', f'{self.epoch}_{self.iters}_{idx}.jpg')
             img_show = denom(inputs)
             inputs = inputs.unsqueeze(0)
             inputs = inputs.to(self.model.device)
@@ -236,18 +248,24 @@ class Trainer():
             cv2.imwrite(image_outname, img_cam)
 
         img_cam = cv2.cvtColor(img_cam, cv2.COLOR_BGR2RGB)
-        self.logger.write_image(f'samples/{self.epoch}_{self.iters}_{idx}', img_cam, step=self.epoch)
+        
+        fig_cam = plt.figure()
+        plt.imshow(img_cam)
+
+        self.logger.write_image(
+            f'samples/{self.epoch}_{self.iters}_{idx}', fig_cam, step=self.epoch)
 
     def logging(self, logs, step):
         tags = [l for l in logs.keys()]
         values = [l for l in logs.values()]
-        self.logger.write(tags= tags, values= values, step=step)
+        self.logger.write(tags=tags, values=values, step=step)
 
     def set_accumulate_step(self):
         self.use_accumulate = False
         if self.cfg.total_accumulate_steps > 0:
             self.use_accumulate = True
-            self.accumulate_steps = max(round(self.cfg.total_accumulate_steps / self.cfg.batch_size), 1) 
+            self.accumulate_steps = max(
+                round(self.cfg.total_accumulate_steps / self.cfg.batch_size), 1)
 
     def set_amp(self):
         self.use_amp = False
@@ -255,13 +273,13 @@ class Trainer():
             self.use_amp = True
 
     def __str__(self):
-        s0 =  "##########   MODEL INFO   ##########"
+        s0 = "##########   MODEL INFO   ##########"
         s1 = "Model name: " + self.model.model_name
         s2 = f"Number of trainable parameters:  {self.model.trainable_parameters():,}"
-       
+
         s5 = "Training iterations per epoch: " + str(len(self.trainloader))
         s6 = "Validating iterations per epoch: " + str(len(self.valloader))
-        return "\n".join([s0,s1,s2,s5,s6])
+        return "\n".join([s0, s1, s2, s5, s6])
 
     def set_attribute(self, kwargs):
         self.checkpoint = None
@@ -274,7 +292,7 @@ class Trainer():
         self.best_value = 0.0
         self.set_accumulate_step()
         self.set_amp()
-        for i,j in kwargs.items():
+        for i, j in kwargs.items():
             setattr(self, i, j)
 
         if self.logger is None:
