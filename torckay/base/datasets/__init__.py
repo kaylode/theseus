28af214c9a3ad5893d03c364d0c3418a3255e474
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-02-12 13:18:16 +0700

    register nn.Dataloader in base

--- torckay/base/datasets/__init__.py
+++ torckay/base/datasets/__init__.py
@@ -1,4 +1,8 @@
 from torckay.registry import Registry
+from torch.utils.data import DataLoader, Dataset
 
 DATASET_REGISTRY = Registry('DATASET')
 DATALOADER_REGISTRY = Registry('DATALOADER')
+
+DATASET_REGISTRY.register(Dataset)
+DATALOADER_REGISTRY.register(DataLoader)
