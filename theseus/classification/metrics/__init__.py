5de6d76e9e25a08d55ab83248f9bb099288293b0
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-02-13 13:07:35 +0700

    add error cases only visualization

--- theseus/classification/metrics/__init__.py
+++ theseus/classification/metrics/__init__.py
@@ -4,9 +4,10 @@ from .accuracy import *
 from .bl_accuracy import *
 from .f1 import *
 from .confusion_matrix import *
-
+from .errorcases import *
 
 METRIC_REGISTRY.register(Accuracy)
 METRIC_REGISTRY.register(BalancedAccuracyMetric)
 METRIC_REGISTRY.register(F1ScoreMetric)
 METRIC_REGISTRY.register(ConfusionMatrix)
+METRIC_REGISTRY.register(ErrorCases)
