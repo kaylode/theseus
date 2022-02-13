1d44bd82d376171110d851c08ddf73feda9ceca9
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-02-13 13:08:20 +0700

    fix download_from_drive function

--- theseus/utilities/download.py
+++ theseus/utilities/download.py
@@ -1,9 +1,12 @@
 import gdown
 
-def download_weights(id_or_url, cached=None, md5=None, quiet=False):
-    if id_or_url.startswith('http'):
+def download_from_drive(id_or_url, output, md5=None, quiet=False, cache=True):
+    if id_or_url.startswith('http') or id_or_url.startswith('https'):
         url = id_or_url
     else:
         url = 'https://drive.google.com/uc?id={}'.format(id_or_url)
 
-    return gdown.cached_download(url=url, path=cached, md5=md5, quiet=quiet)
+    if not cache:
+        return gdown.download(url, output, quiet=quiet)
+    else:
+        return gdown.cached_download(url, cached=output, md5=md5, quiet=quiet)
