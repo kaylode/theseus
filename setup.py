4daf9b4ed1f05dbb644a4248dd08db3308bc890e
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-02-15 17:36:09 +0700

    add setup scripts

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="theseus",
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+    install_requires=[
+        "numpy",
+        "torch",
+        "tensorboard",
+        "albumentations",
+        "torchvision",
+        "tqdm",
+        "timm",
+        "pyyaml>=5.1",
+        "webcolors",
+        "omegaconf",
+        "gdown==3.13.0",
+        "grad-cam",
+    ],
+)
