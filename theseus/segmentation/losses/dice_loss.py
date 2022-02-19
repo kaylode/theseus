4e1fe493ae162a9f96ed6d7fe175133bce081fae
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-02-19 16:04:07 +0700

    use new diceloss

--- theseus/segmentation/losses/dice_loss.py
+++ theseus/segmentation/losses/dice_loss.py
@@ -1,87 +1,26 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-"""
-Source: https://github.com/hubutui/DiceLoss-PyTorch/blob/master/loss.py
-"""
-
-class BinaryDiceLoss(nn.Module):
-    """
-    Dice loss of binary class
-    Args:
-        smooth: A float number to smooth loss, and avoid NaN error, default: 1
-        p: Denominator value: \sum{x^p} + \sum{y^p}, default: 2
-        predict: A tensor of shape [batch_size, W, H]
-        target: A tensor of shape same with predict
-        reduction: Reduction method to apply, return mean over batch if 'mean',
-            return sum if 'sum', return a tensor of shape [batch_size,] if 'none'
-    Returns:
-        Loss tensor according to arg reduction
-    Raise:
-        Exception if unexpected reduction
-    """
-    def __init__(self, smooth=1, p=2, reduction='mean', **kwargs):
-        super(BinaryDiceLoss, self).__init__()
-        self.smooth = smooth
-        self.p = p
-        self.reduction = reduction
-
-    def forward(self, predict , batch, device):
-        target = batch["targets"].to(device)
-
-        assert predict.shape[0] == target.shape[0], "predict & target batch size don't match"
-        predict = predict.contiguous().view(predict.shape[0], -1)
-        target = target.contiguous().view(target.shape[0], -1)
-
-        num = torch.sum(torch.mul(predict, target), dim=1) + self.smooth
-        den = torch.sum(predict.pow(self.p) + target.pow(self.p), dim=1) + self.smooth
-
-        loss = 1 - num / den
-
-        if self.reduction == 'mean':
-            loss = loss.mean()
-        elif self.reduction == 'sum':
-            loss = loss.sum()
-        
-        loss_dict = {"DICE": loss.item()}
-        return loss, loss_dict
-
-
 class DiceLoss(nn.Module):
     """Dice loss, need one hot encode input
-    Args:
-        weight: An array of shape [num_classes,]
-        ignore_index: class index to ignore
-        predict: A tensor of shape [batch_size, num_classes, W, H]
-        target: A tensor of same shape with predict
-        other args pass to BinaryDiceLoss
-    Return:
-        same as BinaryDiceLoss
     """
-    def __init__(self, weight=None, ignore_index=None, **kwargs):
+    def __init__(self, eps=1e-6, **kwargs):
         super(DiceLoss, self).__init__()
-        self.weight = weight
-        self.ignore_index = ignore_index
+        self.eps = eps
 
     def forward(self, predict, batch, device):
-        target = batch["targets"].to(device)
-
-        assert predict.shape == target.shape, 'predict & target shape do not match'
-        dice = BinaryDiceLoss()
-        total_loss = 0
-        predict = F.softmax(predict, dim=1)
-
-        for i in range(target.shape[1]):
-            if i != self.ignore_index:
-                dice_loss, _ = dice(predict[:, i], {'targets': target[:, i]}, device)
-                if self.weight is not None:
-                    assert self.weight.shape[0] == target.shape[1], \
-                        'Expect weight shape [{}], get[{}]'.format(target.shape[1], self.weight.shape[0])
-                    dice_loss *= self.weights[i]
-                total_loss += dice_loss
-
-        loss = total_loss/target.shape[1]
+        targets = batch["targets"].to(device)
+        
+        inputs = F.softmax(predict, dim=1)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice = (2.*intersection + self.eps)/(inputs.sum() + targets.sum() + self.eps)  
+        
+        loss =  1 - dice
 
         loss_dict = {"DICE": loss.item()}
         return loss, loss_dict
