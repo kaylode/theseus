19fa04799a79cf30fffcbebc6938ea95d354e534
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-02-20 23:51:43 +0700

    add lawin model

--- theseus/segmentation/models/__init__.py
+++ theseus/segmentation/models/__init__.py
@@ -2,7 +2,9 @@ from theseus.base.models import MODEL_REGISTRY
 
 from .segformer import SegFormer
 from .segmodels import BaseSegModel
+from .lawin import Lawin
 from .wrapper import ModelWithLoss
 
+MODEL_REGISTRY.register(Lawin)
 MODEL_REGISTRY.register(SegFormer)
 MODEL_REGISTRY.register(BaseSegModel)
