6e30c49ffbad632b045fccd171cb327554b02eee
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-02-20 15:17:24 +0700

    update note in diceloss

--- theseus/segmentation/losses/dice_loss.py
+++ theseus/segmentation/losses/dice_loss.py
@@ -5,24 +5,26 @@ import torch.nn.functional as F
 class DiceLoss(nn.Module):
     """Dice loss, need one hot encode input
     Source: https://github.com/sithu31296/semantic-segmentation/blob/958ed542aa68003eb0a2b0799cf5cecfe6c7587c/semseg/losses.py
+    
+    Note: If Loss becomes NaN, try changing other backbone/model or use different losses
+    DiceLoss won't work everytime due to some activation/normalization layers in the architecture
     """
     def __init__(self, eps=1e-6, **kwargs):
         super(DiceLoss, self).__init__()
         self.eps = eps
 
     def forward(self, predict, batch, device):
-        labels = batch["targets"].to(device)
-        inputs = F.softmax(predict, dim=1)  
+        targets = batch["targets"].to(device)
+        prediction = F.softmax(predict, dim=1)  
 
-        tp = torch.sum(labels*inputs, dim=(2, 3))
-        fn = torch.sum(labels*(1-inputs), dim=(2, 3))
-        fp = torch.sum((1-labels)*inputs, dim=(2, 3))
+        # have to use contiguous since they may from a torch.view op
+        iflat = prediction.contiguous().view(-1)
+        tflat = targets.contiguous().view(-1)
+        intersection = (iflat * tflat).sum()
+        union = prediction.sum() + targets.sum()
 
-        dice_score = (tp + 1e-6) / (tp +  fn + fp + 1e-6)
-        dice_score = torch.sum(1 - dice_score, dim=-1)
+        dice = (2.*intersection + self.eps)/(union + self.eps)  
+        loss =  1 - dice
 
-        loss = dice_score / labels.shape[1]
-        loss = loss.mean()
-        
         loss_dict = {"DICE": loss.item()}
         return loss, loss_dict
