dab2ff86d45fb4c5938c59e2428c46c9ea7eab48
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-02-26 15:40:39 +0700

    fix dice-score, add mean iou

--- theseus/segmentation/metrics/__init__.py
+++ theseus/segmentation/metrics/__init__.py
@@ -2,7 +2,8 @@ from theseus.base.metrics import METRIC_REGISTRY
 
 from .dicecoeff import *
 from .pixel_accuracy import *
-
+from .miou import *
 
 METRIC_REGISTRY.register(PixelAccuracy)
+METRIC_REGISTRY.register(mIOU)
 METRIC_REGISTRY.register(DiceScore)
