ce6c91bf34ccb17b0a95c64cef82c6a0de45d727
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-03-05 00:55:21 +0700

    add wandb to init

--- theseus/utilities/loggers/__init__.py
+++ theseus/utilities/loggers/__init__.py
@@ -2,4 +2,4 @@ from .observer import LoggerObserver
 from .tf_logger import TensorboardLogger
 from .image_writer import ImageWriter
 from .stdout_logger import StdoutLogger, FileLogger
-from .wandb import WandbLogger
+from .wandb_logger import WandbLogger
