ddcd5416c2c29a83ed53c8c4b40b0dd6eb52d131
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-03-27 20:20:04 +0700

    fix losses

--- theseus/classification/losses/ce_loss.py
+++ theseus/classification/losses/ce_loss.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict, Optional
 import torch
 from torch import nn
-from timm.loss import LabelSmoothingCrossEntropy
+from timm.loss import LabelSmoothingCrossEntropy, SoftTargetCrossEntropy
 from theseus.utilities.cuda import move_to
 
 class CELoss(nn.Module):
@@ -28,12 +28,16 @@ class SmoothCELoss(nn.Module):
 
     def __init__(self, smoothing: float=0.1, **kwargs):
         super(SmoothCELoss, self).__init__(**kwargs)
-        self.criterion = LabelSmoothingCrossEntropy(smoothing=smoothing)
+        self.smooth_criterion = LabelSmoothingCrossEntropy(smoothing=smoothing)
+        self.soft_criterion = SoftTargetCrossEntropy()
 
     def forward(self, outputs: Dict[str, Any], batch: Dict[str, Any], device: torch.device):
         pred = outputs['outputs']
         target = move_to(batch["targets"], device)
 
-        loss = self.criterion(pred, target.view(-1).contiguous())
+        if pred.shape == target.shape:
+            loss = self.soft_criterion(pred, target)
+        else:
+            loss = self.smooth_criterion(pred, target.view(-1).contiguous())
         loss_dict = {"CE": loss.item()}
         return loss, loss_dict
