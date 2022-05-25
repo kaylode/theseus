ea0f8e6c95f9897e83dcb35907e65f063bc0fc0b
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-05-25 14:13:04 +0700

    use req.txt in setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,12 @@
 import setuptools
 
+with open("requirements.txt", 'r') as f:
+    reqs = f.read().splitlines()
+
 setuptools.setup(
     name="theseus",
     version='0.0.1',
     packages=setuptools.find_packages(),
     python_requires='>=3.7',
-    install_requires=[
-        "numpy",
-        "torch",
-        "tensorboard",
-        "albumentations>=1.1.0",
-        "torchvision",
-        "tqdm",
-        "timm>=0.5.4",
-        "pyyaml>=5.1",
-        "webcolors",
-        "omegaconf",
-        "gdown>=4.4.0",
-        "grad-cam",
-        "tabulate",
-        "segmentation-models-pytorch",
-        "opencv-python-headless==4.2.0.32",
-        "loguru"
-    ],
+    install_requires=reqs,
 )
