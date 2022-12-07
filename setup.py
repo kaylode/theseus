670db380ec9c3842d793e1f351f91cd627ff6a74
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-12-07 12:54:38 +0000

    update setup with extra packages for efficient installation

--- setup.py
+++ setup.py
@@ -1,12 +1,38 @@
+import re
+import os.path as osp
 import setuptools
+from theseus.utilities.folder import find_file_recursively
 
+
+## Check repo version
+version = ''
+with open('theseus/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+## Base requirements
 with open("requirements.txt", 'r') as f:
-    reqs = f.read().splitlines()
+    install_requires = f.read().splitlines()
+
+## Check extra requirements recursively
+extras_requires = {}
+extra_req_files = find_file_recursively('theseus', filename="requirements.txt")
+for req_file in extra_req_files:
+    with open(req_file, 'r') as f:
+        reqs = f.read().splitlines()
+    pardir = osp.dirname(req_file).split('theseus/')[1].replace('/', '.')
+    extras_requires.update({
+        pardir: reqs,
+    })
 
 setuptools.setup(
     name="theseus",
-    version='0.0.1',
+    author='kaylode',
+    version=version,
+    url='https://github.com/kaylode/theseus.py',
+    license='MIT',
+    description='A project contains templates and useful tools for Deep Learning',
     packages=setuptools.find_packages(),
     python_requires='>=3.7',
-    install_requires=reqs,
+    install_requires=install_requires,
+    extras_require=extras_requires
 )
