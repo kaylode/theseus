d925e1be64126af181c485d5355f65928ad57b36
kaylode <pmkhoi@selab.hcmus.edu.vn>
2022-12-19 17:18:55 +0000

    update registry

--- theseus/tabular/classification/pipeline.py
+++ theseus/tabular/classification/pipeline.py
@@ -3,6 +3,9 @@ from theseus.base.pipeline import BasePipeline
 from theseus.base.utilities.loggers import LoggerObserver
 from theseus.base.utilities.loggers import LoggerObserver, FileLogger, ImageWriter
 from theseus.tabular.classification.callbacks import CALLBACKS_REGISTRY
+from theseus.tabular.classification.metrics import METRIC_REGISTRY
+from theseus.tabular.classification.models import MODEL_REGISTRY
+from theseus.tabular.classification.trainer import TRAINER_REGISTRY
 from theseus.tabular.base.preprocessors import TRANSFORM_REGISTRY
 
 class TabularPipeline(BasePipeline):
@@ -19,6 +22,9 @@ class TabularPipeline(BasePipeline):
         super().init_registry()
         self.callbacks_registry = CALLBACKS_REGISTRY
         self.transform_registry = TRANSFORM_REGISTRY
+        self.model_registry = MODEL_REGISTRY
+        self.metric_registry = METRIC_REGISTRY
+        self.trainer_registry = TRAINER_REGISTRY
         self.logger.text(
             "Overidding registry in pipeline...", LoggerObserver.INFO
         )
