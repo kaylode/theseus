8fa6fd920482babbc84763a739d61f90a1c3c6a0
kaylode <kayp.kieran@gmail.com>
2023-01-02 20:26:22 +0000

    :ambulance: fix bug

--- theseus/cv/detection/datasets/__init__.py
+++ theseus/cv/detection/datasets/__init__.py
@@ -1,4 +1,4 @@
 from .coco import COCODataset
-from theseus.base.datasets import DATASET_REGISTRY
+from theseus.base.datasets import DATASET_REGISTRY, DATALOADER_REGISTRY
 
 DATASET_REGISTRY.register(COCODataset)
