57b96cda05900d7f46e773c2b74b3ddfd1d51703
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-04-02 05:57:41 +0100

    :sparkles: add cml report, edit source to generate metrics.json

--- theseus/base/callbacks/metric_logging_callbacks.py
+++ theseus/base/callbacks/metric_logging_callbacks.py
@@ -1,3 +1,6 @@
+import json
+import os
+import os.path as osp
 from typing import Dict, List
 
 from theseus.base.callbacks.base_callbacks import Callbacks
@@ -13,8 +16,15 @@ class MetricLoggerCallbacks(Callbacks):
         - Only do logging
     """
 
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, save_json: bool = True, **kwargs) -> None:
         super().__init__()
+        self.save_json = save_json
+        if self.save_json:
+            self.save_dir = kwargs.get("save_dir", None)
+            if self.save_dir is not None:
+                self.save_dir = osp.join(self.save_dir, "Validation")
+                os.makedirs(self.save_dir, exist_ok=True)
+            self.output_dict = []
 
     def on_val_epoch_end(self, logs: Dict = None):
         """
@@ -23,6 +33,16 @@ class MetricLoggerCallbacks(Callbacks):
 
         iters = logs["iters"]
         metric_dict = logs["metric_dict"]
+
+        # Save json
+        if self.save_json:
+            item = {}
+            for metric, score in metric_dict.items():
+                if isinstance(score, (int, float)):
+                    item[metric] = float(f"{score:.5f}")
+            if len(item.keys()) > 0:
+                item["iters"] = iters
+                self.output_dict.append(item)
 
         # Log metric
         metric_string = ""
@@ -40,3 +60,13 @@ class MetricLoggerCallbacks(Callbacks):
         ]
 
         LOGGER.log(log_dict)
+
+    def on_finish(self, logs: Dict = None):
+        """
+        After finish everything
+        """
+        if self.save_json:
+            save_json = osp.join(self.save_dir, "metrics.json")
+            if len(self.output_dict) > 0:
+                with open(save_json, "w") as f:
+                    json.dump(self.output_dict, f)
